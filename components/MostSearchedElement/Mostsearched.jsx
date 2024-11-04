635f7f8c51d2619b06d37ef77cd6ac22ffca2c58
satyam <satyamrathoresince26092004@gmail.com>
2024-11-04 22:15:53 +0530

    done

--- components/MostSearchedElement/Mostsearched.jsx
+++ components/MostSearchedElement/Mostsearched.jsx
@@ -88,7 +88,7 @@ const MostSearchedItems = () => {
             <div className="item-card">
               <div className="item-image-container">
                 <img 
-                 src={"/Assets/images/img1.jpg"}
+                src={`/Assets/images2/img${item.id}.jpg`}
                   alt={item.title} 
                   className="item-image" 
                 />
