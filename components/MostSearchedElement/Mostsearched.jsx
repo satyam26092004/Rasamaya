b60576713b4f475da9dcaa8de3fbdb4597768129
satyam <satyamrathoresince26092004@gmail.com>
2024-11-04 21:40:11 +0530

    done

--- components/MostSearchedElement/Mostsearched.jsx
+++ components/MostSearchedElement/Mostsearched.jsx
@@ -88,7 +88,7 @@ const MostSearchedItems = () => {
             <div className="item-card">
               <div className="item-image-container">
                 <img 
-                  src={`/Assets/img${item.id}.png`} // Assuming images are named like img1.png, img2.png, etc.
+                  src="/Users/satyamrathore/Desktop/project1/recepie/Assets/images/img4.jpg"// Assuming images are named like img1.png, img2.png, etc.
                   alt={item.title} 
                   className="item-image" 
                 />
